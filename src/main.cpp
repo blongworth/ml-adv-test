7842adec2fb1f5719812c1e8e0127b5e033bd88e
WHOI-MachineLab <whoimachinelab@gmail.com>
2025-01-08 11:59:04 -0500

    define adv serial

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include <Arduino.h>
 #include "adv.h"
 
-ADV adv(Serial3);
+#define ADV_SERIAL Serial3
+
+ADV adv(ADV_SERIAL);
 
 void setup() {
   Serial.begin(9600);
-  Serial3.begin(115200);
+  ADV_SERIAL.begin(115200);
   delay(3000);
   Serial.println("Teensy ready");
   Serial.println("Starting ADV...");
