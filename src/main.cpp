517b2637ad784503c203dd5be848f0bb748f526e
Brett Longworth <blongworth@whoi.edu>
2025-01-17 12:33:03 -0500

    add loop timer

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 
 ADV adv(ADV_SERIAL);
 
+elapsedMillis loopTimer;
+unsigned long loopCount = 0;
+
 void setup() {
   Serial.begin(9600);
   ADV_SERIAL.begin(115200);
@@ -17,6 +20,16 @@ void setup() {
 }
 
 void loop() {
+  loopCount++;
+  if (loopTimer >= 1000) {
+    Serial.print("Loop rate: ");
+    float loopRate = (float)loopCount / (loopTimer / 1000.0);
+    Serial.print(loopRate);
+    Serial.println(" loops per second");
+    loopCount = 0;
+    loopTimer = 0;
+  }
+  
   adv.read();
   adv.getVVDPacket();
   adv.getVSDPacket();
